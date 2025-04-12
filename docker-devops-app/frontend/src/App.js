b5496314903b9a36a1ab9d67d68de71f627280cd
saikrupaelate <161908072+saikrupaelate@users.noreply.github.com>
2025-04-12 19:02:47 -0400

    Update App.js from localhost to backend

--- docker-devops-app/frontend/src/App.js
+++ docker-devops-app/frontend/src/App.js
@@ -4,7 +4,7 @@ function App() {
   const [data, setData] = useState('');
 
   useEffect(() => {
-    fetch('http://localhost:5000').then(res => res.text()).then(setData);
+    fetch('http://backend:5000').then(res => res.text()).then(setData);
   }, []);
 
   return <div><h1>{data}</h1></div>;
