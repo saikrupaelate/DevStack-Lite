7a99af62d6ac9a0bcdfff48cc068353fa796b78b
saikrupaelate <161908072+saikrupaelate@users.noreply.github.com>
2025-04-13 19:52:57 -0400

    Update App.js

--- docker-devops-app/frontend/src/App.js
+++ docker-devops-app/frontend/src/App.js
@@ -4,7 +4,7 @@ function App() {
   const [data, setData] = useState('');
 
   useEffect(() => {
-    fetch('http://backend:5000').then(res => res.text()).then(setData);
+    fetch(`${process.env.REACT_APP_BACKEND_URL}`).then(res => res.text()).then(setData);
   }, []);
 
   return <div><h1>{data}</h1></div>;
